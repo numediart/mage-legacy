8370f9dcab1f3e109a96c2fe495ca7ef92d4545e
Alexis Moinet <alexis.moinet@umons.ac.be>
2012-07-19 17:37:59 +0200

    apply changes to osx version of app
    - migrate to osx too
    - fix memory leaks in Model.cpp
    - some minor stuffs

--- examples/of_0071_linux_release/apps/enterface2012/testApp.cpp
+++ examples/of_0071_linux_release/apps/enterface2012/testApp.cpp
@@ -126,8 +126,12 @@ void testApp::keyPressed( int key ) {
     if( key == 'l' ) {
         
         //MAGE::Label label("x^x-pau+ae=l@x_x/A:0_0_0/B:x-x-x@x-x&x-x#x-x$x-x!x-x;x-x|x/C:1+1+2/D:0_0/E:x+x@x+x&x+x#x+x/F:content_2/G:0_0/H:x=x^1=10|0/I:19=12/J:79+57-10");
-        //MAGE::Label label;
-        //label.setQuery( "x^x-pau+ae=l@x_x/A:0_0_0/B:x-x-x@x-x&x-x#x-x$x-x!x-x;x-x|x/C:1+1+2/D:0_0/E:x+x@x+x&x+x#x+x/F:content_2/G:0_0/H:x=x^1=10|0/I:19=12/J:79+57-10" );
+//        MAGE::Label label;
+//        label.setQuery( "x^x-pau+ae=l@x_x/A:0_0_0/B:x-x-x@x-x&x-x#x-x$x-x!x-x;x-x|x/C:1+1+2/D:0_0/E:x+x@x+x&x+x#x+x/F:content_2/G:0_0/H:x=x^1=10|0/I:19=12/J:79+57-10" );
+//        label.setQuery( "x^pau-ae+l=ax@1_2/A:0_0_0/B:1-1-2@1-2&1-19#1-10$1-5!0-2;0-8|ae/C:0+0+2/D:0_0/E:content+2@1+12&1+6#0+2/F:aux_1/G:0_0/H:19=12^1=10|L-H%/I:3=3/J:79+57-10" );
+//        if( !labelQueue->isFull() ) labelQueue->push( label );
+//        else printf( "label queue is full !\n");
+        
         MAGE::Label label;
         while (!labellist.empty()) {
             string q = labellist.front();
